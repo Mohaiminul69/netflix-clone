fd49a6a455c0b7632acf7b9a17603a50ff3d0a97
mohaiminul69 <mohaiminul69@gmail.com>
2022-10-09 14:48:58 +0600

    user login implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./Screens/HomeScreen/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./Screens/Login/LoginScreen";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log(uid);
+      } else {
+      }
+    });
+    return unsubscribe;
+  }, []);
   return (
     <div className="App">
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDy6zVEKUzClXX30EtoUFupbNIdEAHuERw",
   authDomain: "netflix-clone-fb79e.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "56532567595",
   appId: "1:56532567595:web:e03723862080fdd9698c6e",
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+export { auth };
+export default db;
