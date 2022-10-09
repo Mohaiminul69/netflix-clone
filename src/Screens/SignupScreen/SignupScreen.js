329c92680ce5a8c381563d2ba3be9d01e48a52e8
mohaiminul69 <mohaiminul69@gmail.com>
2022-10-09 13:49:23 +0600

    signup UI done

--- src/Screens/SignupScreen/SignupScreen.js
+++ src/Screens/SignupScreen/SignupScreen.js
@@ -2,9 +2,27 @@ import React from "react";
 import "./SignupScreen.css";
 
 const SignupScreen = () => {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="password" />
+        <button onClick={(e) => signIn(e)} type="submit">
+          Sign In
+        </button>
+        <h4>
+          New to Netflix? <span onClick={(e) => register(e)}>Sign up now.</span>
+        </h4>
+      </form>
     </div>
   );
 };
