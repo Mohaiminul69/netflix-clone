5a8faab5c0d7df29a594f8574d61dca7ca6e8d5b
mohaiminul69 <mohaiminul69@gmail.com>
2022-10-09 17:36:49 +0600

    user auth seting done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./Screens/Login/LoginScreen";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        const uid = user.uid;
-        console.log(uid);
+        dispatch(
+          login({
+            uid: user.uid,
+            email: user.email,
+          })
+        );
       } else {
+        dispatch(logout);
       }
     });
     return unsubscribe;
